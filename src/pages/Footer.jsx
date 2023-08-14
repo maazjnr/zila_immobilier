55c737888a3e098ea6dd4afd3e857837a09f90a4
goalmaaz@gmail.com <goalmaaz@gmail.com>
2023-08-14 15:50:27 +0100

    updates

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,12 +1,109 @@
-import React from 'react';
-import './Footer.Style.css';
+import React from "react";
+import "./Footer.Style.css";
 
- function Footer() {
+function Footer() {
   return (
-    <div className='footer-container'>
-        <h1>Footer</h1>
+    <div className="footer-container w-full flex flex-wrap justify-between md:justify-between p-4 md:p-8">
+      <section className="w-full md:w-1/5 mb-4 md:mb-0 flex flex-col ">
+        <h1
+          className="text-secondary  tracking-wider font-primary 
+        text-md font-normal"
+        >
+          Nextovee
+        </h1>
+
+        <div className="flex flex-col py-3">
+          <a className="text-[#ffffff] text-xs py-1 font-primary">
+            Explore for sale properties
+          </a>
+          <a className="text-[#ffffff] text-xs py-1 font-primary">
+            Explore for rent properties
+          </a>
+          <a className="text-[#ffffff] text-xs py-1 font-primary">
+            Buy properties
+          </a>
+          <a className="text-[#ffffff] text-xs py-1 font-primary">See prices</a>
+          <a className="text-[#ffffff] text-xs py-1 font-primary">
+            Sign in / Create account
+          </a>
+          <a className="text-[#ffffff] text-xs py-1 font-primary">Blog</a>
+        </div>
+      </section>
+
+      <section className="w-full md:w-1/5 mb-4 md:mb-0 flex flex-col ">
+        <h1
+          className="text-secondary tracking-wider font-primary 
+                    text-md font-normal"
+        >
+          Checkout Resources
+        </h1>
+
+        <div className="flex flex-col py-3">
+          <a className="text-[#ffffff] text-xs py-1 font-primary">
+            Looking for a place to live?
+          </a>
+          <a className="text-[#ffffff] text-xs py-1 font-primary">Schools</a>
+          <a className="text-[#ffffff] text-xs py-1 font-primary">Students</a>
+          <a className="text-[#ffffff] text-xs py-1 font-primary">See prices</a>
+          <a className="text-[#ffffff] text-xs py-1 font-primary">
+            Property guides help
+          </a>
+          <a className="text-[#ffffff] text-xs py-1 font-primary">
+            Help to buy property
+          </a>
+        </div>
+      </section>
+
+      <section className="w-full md:w-1/5 mb-4 md:mb-0 flex flex-col ">
+        <h1
+          className="text-secondary tracking-wider font-primary 
+                    text-md font-normal"
+        >
+          Quick links access
+        </h1>
+
+        <div className="flex flex-col py-3">
+          <a className="text-[#ffffff] text-xs py-1 font-primary">
+            Property investment
+          </a>
+          <a className="text-[#ffffff] text-xs py-1 font-primary">
+            Find an agent
+          </a>
+          <a className="text-[#ffffff] text-xs py-1 font-primary">
+            Cheap house valuable for sale
+          </a>
+        </div>
+      </section>
+
+      <section className="w-full md:w-1/5 mb-4 md:mb-0 flex flex-col ">
+        <h1
+          className="text-secondary tracking-wider font-primary 
+                    text-md font-normal"
+        >
+          Location
+        </h1>
+
+        <div className="flex flex-col py-3">
+          <a className="text-[#ffffff] text-xs py-1 font-primary">England</a>
+          <a className="text-[#ffffff] text-xs py-1 font-primary">Nigeria</a>
+          <a className="text-[#ffffff] text-xs py-1 font-primary">Scotland</a>
+          <a className="text-[#ffffff] text-xs py-1 font-primary">Ghana</a>
+        </div>
+      </section>
+
+      <img
+            alt="logo"
+            style={{
+              height: 55,
+              width: 100,
+              marginTop: 30,
+              paddingBottom: "2rem",
+            }}
+            src={require("../images/Nextmovee.png")}
+            width={80}
+          />
     </div>
-  )
+  );
 }
 
 export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,12 @@ import Recommend from "../components/Recommend/Recommend";
 import { MdSell } from "react-icons/md";
 import { FaBookmark } from "react-icons/fa";
 import { AiFillShopping } from "react-icons/ai";
-import {BiSearch} from "react-icons/bi";
-
+import { BiSearch } from "react-icons/bi";
 
 export default function Home() {
-  
   return (
     <main className="flex flex-col items-center ">
-      <div className="flex home-bg flex-col items-center p-5 ">
-
-
+      <div className="flex home-bg flex-col items-center p-3 ">
         <div className="flex flex-col items-center mt-5 relative z-10">
           <section className="text-center">
             <h1
@@ -29,104 +25,102 @@ export default function Home() {
             </p>
           </section>
           <div className="w-full mt-6 relative">
-  <input
-    type="text"
-    placeholder="Search for your dream home..."
-    className="border shadow-lg rounded px-5 py-3 w-full bg-white"
-  />
-  <div className="absolute inset-y-0 right-0 flex items-center mr-2">
-    <button style={{backgroundColor: '#aeaeae'}}
-      className="bg-gray rounded px-3 py-2 text-white focus:outline-none"
-    >
-      <BiSearch color='white' className="h-5 w-5" />
-    </button>
-  </div>
-</div>
-
-
-
-  
+            <input
+              type="text"
+              placeholder="Search for your dream home..."
+              className="border shadow-lg rounded px-5 py-3 w-full bg-white"
+            />
+            <div className="absolute inset-y-0 right-0 flex items-center mr-2">
+              <button
+                style={{ backgroundColor: "#aeaeae" }}
+                className="bg-gray rounded px-3 py-2 text-white focus:outline-none"
+              >
+                <BiSearch color="white" className="h-5 w-5" />
+              </button>
+            </div>
+          </div>
         </div>
 
         <div className="mt-5 mb-8 flex flex-wrap justify-evenly ">
-          <button
+          <button className="home-btn"
             style={{
-              backgroundColor: "#3A3845",
+              backgroundColor: "#fff",
               color: "#ffffff",
               borderRadius: "0.25rem",
               fontWeight: "600",
               fontSize: "15px",
               margin: "5px",
-              width: "80px",
+              width: "90px",
               height: "40px",
               display: "flex",
               justifyContent: "center",
               alignItems: "center",
+              
             }}
           >
-            <MdSell />
-            <p className="ml-2 text-secondary">BUY</p>
+            <MdSell color={'#3A3845'} />
+            <p className="ml-0 text-[#3A3845]">BUY</p>
           </button>
-          <button
+          <button className="home-btn"
             style={{
-              backgroundColor: "#3A3845",
+              backgroundColor: "#fff",
               color: "#ffffff",
               borderRadius: "0.25rem",
               fontWeight: "600",
               fontSize: "15px",
               margin: "5px",
-              width: "80px",
+              width: "90px",
               height: "40px",
               display: "flex",
               justifyContent: "center",
               alignItems: "center",
             }}
           >
-            <AiFillShopping />
-            <p className="ml-2 text-secondary "> SELL</p>
+            <AiFillShopping  color={'#3A3845'} />
+            <p className="ml-0 text-[#3A3845] "> SELL</p>
           </button>
-          <button
+          <button className="home-btn"
             style={{
-              backgroundColor: "#3A3845",
+              backgroundColor: "#fff",
               color: "#ffffff",
               borderRadius: "0.25rem",
               fontWeight: "600",
               fontSize: "15px",
               margin: "5px",
-              width: "80px",
+              width: "90px",
               height: "40px",
               display: "flex",
               justifyContent: "center",
               alignItems: "center",
             }}
           >
-            <FaBookmark />
-            <p className="ml-2 text-secondary"> RENT</p>
+            <FaBookmark color={'#3A3845'} />
+            <p className="ml-0 text-[#3A3845]"> RENT</p>
           </button>
         </div>
 
-        <section className="mt-5 px-5 ">
-        <div className="text-center ">
-          <h3 className="text-center font-bold mb-0 text-primary uppercase bg-gray-200 p-2">
-            Why Nextmovee
-          </h3>
+        <section className="mt-5 px-2 ">
+          <div className="text-center ">
+            <h3 className="text-center font-bold mb-0 text-primary uppercase bg-gray-200 p-2">
+              Why Nextmovee
+            </h3>
 
-          <p className="text-md text-center text-justify text-primary">
-            At nextomovee, we're more than just a real estate service – we're
-            your trusted partner in finding the perfect property. Our mission is
-            simple: we recommend the best price, the ideal location, and a safe
-            environment tailored to your needs.
-     
-          </p>
+            <p className="text-md text-center text-justify text-primary">
+              At nextomovee, we're more than just a real estate service – we're
+              your trusted partner in finding the perfect property. Our mission
+              is simple: we recommend the best price, the ideal location, and a
+              safe environment tailored to your needs.
+            </p>
 
-          <p className="text-md text-center text-justify text-primary mt-3">       Join us in your real estate journey, and experience the difference
-            of personalized recommendations that prioritize your needs. Your
-            dream property is out there, and we're here to help you find it."</p>
-        </div>
-      </section>
+            <p className="text-md text-center text-justify text-primary mt-3">
+              {" "}
+              Join us in your real estate journey, and experience the difference
+              of personalized recommendations that prioritize your needs. Your
+              dream property is out there, and we're here to help you find it."
+            </p>
+          </div>
+        </section>
       </div>
-
-
 
       {/* Recommend component */}
       <Recommend />
