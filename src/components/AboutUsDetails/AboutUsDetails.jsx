55f883f21a381be44f7febc477e01abe6e2b1894
goalmaaz@gmail.com <goalmaaz@gmail.com>
2023-08-17 14:35:04 +0100

    updates

---
+++ src/components/AboutUsDetails/AboutUsDetails.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+function AboutUsDetails() {
+  return (
+    <main className="w-full flex flex-wrap  justify-between md:justify-between mt-10 p-4 md:p-8 ">
+
+      <div className="mt-3 w-full md:w-1/4 mb-4 md:mb-0 flex flex-col px-3">
+        <h1 className="text-primaryx font-bold text-lg">Mission</h1>
+        <p className="text-sm pt-2 leading-[1.8] text-left text-justify  text-primary">
+          At NextMovee, our mission is to empower individuals and families to
+          find their next perfect home effortlessly. We are dedicated to
+          utilizing our expertise and resources to streamline the real estate
+          journey, providing innovative solutions that enhance the lives of our
+          clients while maintaining a commitment to transparency, trust, and
+          excellence.
+        </p>
+      </div>
+
+      <div className="mt-3 w-full md:w-1/4 mb-4 md:mb-0 flex flex-col px-3">
+        <h1 className="text-primaryx font-bold text-lg">Vision</h1>
+        <p className="text-sm pt-2 leading-[1.8] text-left text-justify text-primary">
+          Our vision at NextMovee is to redefine the real estate experience,
+          becoming the ultimate destination where dreams of homeownership come
+          to life. We aim to be the most trusted and sought-after real estate
+          partner, recognized for our unwavering dedication to delivering
+          personalized service, cutting-edge technology, and a seamless, joyful
+          path to finding your next move.
+        </p>
+      </div>
+
+      <div className="mt-3 w-full md:w-1/4 mb-4 md:mb-0 flex flex-col px-3">
+        <h1 className="text-primaryx font-bold text-lg">Resources</h1>
+        <p className="text-sm pt-2 leading-[1.8] text-left text-justify text-primary">
+          Backed by a team of seasoned real estate professionals, NextMovee
+          harnesses the power of market insights, advanced technology, and
+          industry connections to offer an unparalleled selection of properties.
+          Our comprehensive database, combined with our commitment to staying
+          ahead of market trends, ensures that our clients have access to the
+          widest range of options when making their next move.
+        </p>
+      </div>
+
+      <div className=" mt-3 w-full md:w-1/4 mb-4 md:mb-0 flex flex-col px-3">
+        <h1 className="text-primaryx font-bold text-lg">Our values</h1>
+        <p className="text-sm pt-2 leading-[1.8] text-left text-justify  text-primary">
+          Integrity: We uphold honesty and transparency in all our interactions,
+          building trust through ethical business practices. Customer-Centric:
+          Our clients are our priority. We personalize our services to exceed
+          their expectations and make their dreams a reality. Innovation: We
+          embrace change and leverage cutting-edge technology to enhance the
+          real estate process for our clients. Collaboration: We believe in the
+          power of teamwork, fostering cooperation to deliver exceptional
+          results for our clients. Excellence: Striving for the best is
+          ingrained in our culture. We consistently raise the bar to provide
+          top-notch services and experiences. Community: We are committed to
+          giving back, contributing positively to the communities we serve and
+          creating a lasting impact.
+        </p>
+      </div>
+    </main>
+  );
+}
+
+export default AboutUsDetails;
--- src/pages/About_Us.jsx
+++ src/pages/About_Us.jsx
@@ -1,31 +1,58 @@
 import React from "react";
+import AboutUsDetails from "../components/AboutUsDetails/AboutUsDetails";
 
 function About_Us() {
   return (
-    <main className="about-container">
-      <div
-        style={{ zIndex: 3, textAlign: "center", width: "100%", padding: 20 }}
-        className=""
-      >
-        <h1 className="about-title">More About us</h1>
-        <h1 className="text-xs text-center text-sm text-[#fff]">
-          We help clients buy and sell houses since 2019
-        </h1>
+    <main className="">
+      <section className="about-container">
         <div
-          style={{
-            height: 0.1,
-            marginTop: 40,
-            marginBottom: 5,
-            width: "100%",
-            backgroundColor: "#F7CCAC",
-          }}
-        ></div>
-        <p className="px-3 text-[#fff] text-xs font-hairline text-secondary text-center ">
-          With over $ Billion in sales, due to our great results, expertise and
-          dedication, we rank amongst the best globally in property sales
-          business. Our agency is the industry are among the best.
-        </p>
-      </div>
+          style={{ zIndex: 3, textAlign: "center", width: "100%", padding: 20 }}
+          className=""
+        >
+          <h1 className="about-title">More About us</h1>
+          <h1 className="text-xs text-center text-sm text-[#fff]">
+            We help clients buy and sell houses since 2019
+          </h1>
+          <div
+            style={{
+              height: 0.1,
+              marginTop: 40,
+              marginBottom: 5,
+              width: "100%",
+              backgroundColor: "#F7CCAC",
+            }}
+          ></div>
+          <p className="px-3 text-[#fff] text-xs font-hairline text-secondary text-center ">
+            With over $ Billion in sales, due to our great results, expertise
+            and dedication, we rank amongst the best globally in property sales
+            business. Our agency is the industry are among the best.
+          </p>
+        </div>
+      </section>
+
+      <section className="about-our-container mt-10 text-center">
+        <div className="px-5">
+          <h1 className="text-md text-center py-1 font-bold text-sm text-primary">More about our Company</h1>
+          <p className="text-sm  text-secondaryx leading-[1.8] text-left text-justify px-2 ">
+            Where Vision Meets Home: Crafting Your Perfect Space with Expertise
+            and Care. Discover the Pinnacle of Real Estate Excellence, Tailored
+            to Your Aspirations and Lifestyle.
+          </p>
+        </div>
+
+        <div
+            style={{
+              height: 0.1,
+              marginTop: 40,
+              marginBottom: 5,
+              width: "100%",
+              backgroundColor: "#F7CCAC",
+            }}
+          ></div>
+
+        <AboutUsDetails />
+
+      </section>
     </main>
   );
 }
